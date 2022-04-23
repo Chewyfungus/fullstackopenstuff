321bc31758e208f3835cfdfae46a8f135531bd04
Ayla <collinmaxjohnson@gmail.com>
2022-04-22 21:56:54 -0700

    1.12

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = ({ onClick, text }) => (
+	<button onClick={onClick}>
+		{text}
+	</button>
+)
+
+const Display = (props) => {
+  const text = props.text
+  return (<h1>{text}</h1>)
 }
 
-export default App;
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const getSelected = () => {
+    setSelected(Math.floor(Math.random() * (anecdotes.length)))
+  }
+
+  console.log(selected)
+  console.log([anecdotes[selected]])
+
+  return (
+    <div>
+      <Display text={anecdotes[selected]} />
+      <Button onClick={getSelected} text="get quote" />
+    </div>
+
+  )
+}
+
+export default App
